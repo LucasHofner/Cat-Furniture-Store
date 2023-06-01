1a986ebaee36257c5307b0425cf6478ac6fcb52b
Lucas Hofner <lucashofner@gmail.com>
2023-06-01 12:25:50 -0300

    - Implementing find,create and delete endpoints for post. - Changing type of column "post_date" to DateTime, including the time on it.

--- api/src/main/java/br/com/kapatinhas/api/controllers/PostController.java
+++ api/src/main/java/br/com/kapatinhas/api/controllers/PostController.java
@@ -1,8 +1,12 @@
 package br.com.kapatinhas.api.controllers;
 
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,18 +15,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kapatinhas.api.model.dto.PostDTO;
+import br.com.kapatinhas.api.model.entity.Post;
+import br.com.kapatinhas.api.model.mapper.PostMapper;
+import br.com.kapatinhas.api.repository.PostRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/post")
 public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostMapper postMapper;
     
-    @GetMapping("/find-all")
-    public ResponseEntity<PostDTO> findPosts(@RequestParam LocalDateTime fromDate, @RequestParam LocalDateTime toDate){
-        return null;
+    @GetMapping("/find")
+    public ResponseEntity<List<PostDTO>> findPosts(){
+        return ResponseEntity.ok(postRepository
+                    .findAll()
+                    .stream()
+                    .map(postMapper::convertToDto)
+                    .collect(Collectors.toList()));
     }
 
     @PostMapping("/create")
-    public void createPost(@RequestBody PostDTO post){
-        System.out.println(post);
+    public void createPost(@RequestBody @Valid PostDTO postDto){
+        Post post = postMapper.convertToEntity(postDto);
+        postRepository.saveAndFlush(post);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<PostDTO> deletePost(@RequestParam Long postId){
+        Optional<Post> postToBeDeletedOpt = postRepository.findById(postId);
+
+        if(!postToBeDeletedOpt.isPresent()) return ResponseEntity.notFound().build();
+
+        Post postToBeDeleted = postToBeDeletedOpt.get();
+        postRepository.delete(postToBeDeleted);
+
+        return ResponseEntity.ok(
+            postMapper.convertToDto(postToBeDeleted)
+        );
     }
 }
